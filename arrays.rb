ef82921314892958dd1cbd18736ce639fd2c4c48
Bryan Keener <bkeener7@gmail.com>
2022-08-03 09:34:52 -0600

    Add four calls to main arrays

--- arrays.rb
+++ arrays.rb
@@ -3,3 +3,11 @@ friends = ["Brennan", "Bryan", "Justin", "Greg"]
 age = [32, 31, 30, 32]
 height = [6.1, 5.11, 5.9, 6.0]
 lives_Texas = [false, false, false, true]
+#This call eliminates Greg from the `friends` array
+friends.pop
+#This call adds 30 to the end of the `age` array
+age.push(30)
+#This call eliminates 6.1 from the `height` array
+height.shift
+#This call adds true to the start of the `lives_Texas` array
+lives_Texas.unshift (true)
